50b2b71f8120ee81a0214092e7533fa7b459f375
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-11 17:25:12 +0530

    task changes

--- context-api-task/src/main.jsx
+++ context-api-task/src/main.jsx
@@ -1,9 +1,10 @@
-import {  StrictMode, useContext } from 'react'
+import {  StrictMode,  } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createContext } from 'react'
 
-let mainobject = useContext(  )
+let mainobject = createContext()
 
 let data = {
   name:"shakti",
