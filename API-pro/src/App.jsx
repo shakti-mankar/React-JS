0a5c34507ad5da34a29a6ba50a37e7826589ed73
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-13 16:43:53 +0530

    api detching

--- API-pro/src/App.jsx
+++ API-pro/src/App.jsx
@@ -1,6 +1,60 @@
-function App(){
-  return(
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-    <h1> This is app page </h1>
+
+function App(){
+
+ 
+ 
+ let [apidata,setapidata] = useState([])
+
+
+
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/comments')
+    .then((res)=>setapidata(res.data))
+
+  },[])
+
+
+  return(
+    <>
+    
+    <h1>Table</h1>
+
+    <table border="">
+      <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>BODY</th>
+
+      </tr>
+
+      {
+        apidata.map((e)=>(
+
+          <tr>
+            <td> {e.id} </td>
+            <td> {e.name} </td>
+            <td> {e.email} </td>
+            <td> {e.body} </td>
+
+          </tr>
+          
+        ))
+      }
+
+
+    </table>
+
+
+
+
+    
+    </>
   )
 }
+
+export default App
