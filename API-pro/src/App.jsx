5aa05e16bd54ba20bc414dfa00a8faa3838f7776
Shakti-mankar <mailshaktimankar@gmail.com>
2026-02-13 22:37:52 +0530

    api

--- API-pro/src/App.jsx
+++ API-pro/src/App.jsx
@@ -1,50 +1,114 @@
+// import axios from "axios"
+// import { useEffect, useState } from "react"
+
+
+// function App(){
+
+ 
+ 
+//  let [apidata,setapidata] = useState([])
+
+
+
+
+//   useEffect(()=>{
+//     axios.get('https://jsonplaceholder.typicode.com/comments')
+//     .then((res)=>setapidata(res.data))
+
+//   },[])
+
+
+//   return(
+//     <>
+    
+//     <h1>Table</h1>
+
+//     <table border="">
+//       <tr>
+//         <th>ID</th>
+//         <th>NAME</th>
+//         <th>EMAIL</th>
+//         <th>BODY</th>
+
+//       </tr>
+
+//       {
+//         apidata.map((e)=>(
+
+//           <tr>
+//             <td> {e.id} </td>
+//             <td> {e.name} </td>
+//             <td> {e.email} </td>
+//             <td> {e.body} </td>
+
+//           </tr>
+          
+//         ))
+//       }
+
+
+//     </table>
+
+
+
+
+    
+//     </>
+//   )
+// }
+
+// export default App
+
 import axios from "axios"
 import { useEffect, useState } from "react"
 
+function App() {
 
-function App(){
-
- 
- 
- let [apidata,setapidata] = useState([])
-
-
-
+  let [apidata,setapidata] = useState([])
 
   useEffect(()=>{
     axios.get('https://jsonplaceholder.typicode.com/comments')
     .then((res)=>setapidata(res.data))
-
   },[])
 
 
-  return(
-    <>
-    
-    <h1>Table</h1>
 
-    <table border="">
+
+
+  return(
+
+    <>
+
+    <h1> This is App page </h1>
+
+    <table border="" >
       <tr>
         <th>ID</th>
         <th>NAME</th>
         <th>EMAIL</th>
         <th>BODY</th>
-
+        
       </tr>
 
       {
         apidata.map((e)=>(
 
-          <tr>
-            <td> {e.id} </td>
-            <td> {e.name} </td>
-            <td> {e.email} </td>
-            <td> {e.body} </td>
+        <tr>
+        <td> {e.id} </td>
+        <td> {e.name} </td>
+        <td> {e.email} </td>
+        <td> {e.body} </td>
 
-          </tr>
+
+      </tr>
+
           
+
+
+
         ))
       }
+
 
 
     </table>
@@ -54,6 +118,8 @@ function App(){
 
     
     </>
+
+
   )
 }
 
